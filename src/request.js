21f166ef5df8a0ce34e649310fb5dd3c38d6758e
Frank Crook <frank.crook@gmail.com>
2012-01-28 21:23:41 -1000

    Completely removed fs operations. They were hanging around from when I was considering making this something larger.

--- src/request.js
+++ src/request.js
@@ -1,31 +1,23 @@
-var fs = require('fs'),
-    crypto = require('crypto'),
+var crypto = require('crypto'),
     events = require('events');
 
 var emitter = new events.EventEmitter()
 emitter.setMaxListeners(0);
 
+var hash = '';
+
 function submit(postData, response) {
     console.log("Request handler 'submit' was called.");
     var shaSum = crypto.createHash('sha1');
     shaSum.update(postData);
-    fs.writeFile('hash', shaSum.digest('hex'));
+    hash = shaSum.digest('hex');
     emitter.emit('submitted');
 }
 
 //retrieve immediately returns the text file
 function retrieve(postData, response) {
-    fs.readFile('hash', function(error, data) {
-        if (error) {
-            fs.writeFile('hash', '');
-            response.writeHead(500, {'Content-Type': 'text'});
-            response.end();
-        }
-        else {
-            response.writeHead(200, {'Content-Type': 'text'});
-            response.end(data);
-        }
-    });
+    response.writeHead(200, {'Content-Type': 'text'});
+    response.end(hash);
 }
 
 //longPoll sends the text file when a change is detected
@@ -33,14 +25,7 @@ function longPoll(postData, response) {
     console.log("Request handler 'longPoll' was called.");
     response.writeHead(200, {'Content-Type': 'text'});
     emitter.on('submitted', function() {
-        fs.readFile('hash', function(error, data) {
-            if (error) {
-                fs.writeFile('hash', '');
-                response.end();
-            } else {
-                response.end(data);
-            }
-        });
+        response.end(hash);
     });
 }
 

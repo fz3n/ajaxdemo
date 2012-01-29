d6c8feab7cb9c308887e3ce108f90457f36ad496
Frank Crook <frank.crook@gmail.com>
2012-01-28 21:40:05 -1000

    Cleaned up the very ugly JS I smashed onto the keyboard last night.

--- src/website/js/script.js
+++ src/website/js/script.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
+
+    //lets the user submit a hash to the server
     $("#submission").submit(function(event) {
         event.preventDefault(); //don't submit the form
         var $form =  $(this), 
             submit = $form.find('input[name="input"]').val(),
             url =    $form.attr('action');
-        
-        $.post(url, {input: submit},
-           function(data) {
-               $("#hashes").val(submit);
-        ;});
+        $.post(url, {input: submit});
     });
 
-    window.onload = function() {
-        function retrieve() {
-            $.ajax({
-                url: 'retrieve',
-                dataType: 'text',
-                success: function(data) {
-                    $("#hash").val(data);
-                }
-            });
-        }
-        retrieve();
+    //gets the hash the first time, before it changes
+    function retrieve() {
+        $.ajax({
+            url: 'retrieve',
+            dataType: 'text',
+            success: function(data) {
+                $("#hash").val(data);
+            }
+        });
+    }
 
-        function longPoll() {
-            $.ajax({
-                url: 'longPoll',
-                dataType: 'text',
-                success: function(data) {
-                    $("#hash").val(data);
-                    longPoll();
-                },
-                error: function() {
-                    //if there is an error, wait 5 seconds and try again
-                    setTimeout(longPoll, 5*1000);
-                }
-            });
-        }
+    //loads the hash on a change
+    function longPoll() {
+        $.ajax({
+            url: 'longPoll',
+            dataType: 'text',
+            success: function(data) {
+                $("#hash").val(data);
+                longPoll();
+            },
+            error: function() {
+                //if there is an error, wait 5 seconds and try again
+                setTimeout(longPoll, 5 * 1000);
+            }
+        });
+    }
+    window.onload = function() {
+        retrieve();
         longPoll();
     }
 
